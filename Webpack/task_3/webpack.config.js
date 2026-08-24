1a43327d1c79c3730ca816c7279151978cf95611
SpacePistache <lukeholley@hotmail.com>
2026-08-24 13:08:16 +0200

    modified webpack config and removed pathname

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   devtool: 'inline-source-map',
   devServer: {
-    static: path.resolve(__dirname, 'public'),
+    static: '/public',
     port: 8564,
     open: true,
   },
