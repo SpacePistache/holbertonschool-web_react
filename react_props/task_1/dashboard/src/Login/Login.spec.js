ba18402c7b380877aaa538284ec947dce2840b5e
SpacePistache <lukeholley@hotmail.com>
2026-08-27 11:27:28 +0200

    forgot to update old footer text

---
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Login from './Login';
+
+describe('Login component', () => {
+  test('renders 2 labels, 2 inputs and 1 button', () => {
+    const { container } = render(<Login />);
+
+    expect(container.querySelectorAll('label')).toHaveLength(2);
+    expect(container.querySelectorAll('input')).toHaveLength(2);
+    expect(container.querySelectorAll('button')).toHaveLength(1);
+  });
+
+  test('focuses the input when its label is clicked', async () => {
+    const user = userEvent.setup();
+    render(<Login />);
+
+    await user.click(screen.getByText(/email/i));
+    expect(screen.getByLabelText(/email/i)).toHaveFocus();
+
+    await user.click(screen.getByText(/password/i));
+    expect(screen.getByLabelText(/password/i)).toHaveFocus();
+  });
+});
