fd423d27c3327d72697f90a3aef570c27402f591
SpacePistache <lukeholley@hotmail.com>
2026-08-27 12:39:29 +0200

    made NotificationItem.jsx  accept plain html

--- react_props/task_2/dashboard/src/Notifications/NotificationItem.jsx
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.jsx
@@ -2,11 +2,12 @@ function NotificationItem({ type = 'default', html, value }) {
   const style = { color: type === 'urgent' ? 'red' : 'blue' };
 
   if (html) {
+    const content = typeof html === 'string' ? { __html: html } : html;
     return (
       <li
         data-notification-type={type}
         style={style}
-        dangerouslySetInnerHTML={html}
+        dangerouslySetInnerHTML={content}
       />
     );
   }
